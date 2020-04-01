4898d926e64106408fe0681d72dae150a47a6dab
erfei <1453675918@qq.com>
2020-04-01 17:49:49 +0800

    静态资源部署

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
 
-    return '<h1>hello Flask 111</h1>'
+    name = 'Erfei'
+    movies = [
+        {'title': '大赢家', 'year': '2020'},
+        {'title': '海贼王-顶上战争', 'year': '2019'},
+        {'title': '囧妈', 'year': '2020'},
+        {'title': '速度与激情8', 'year': '2019'},
+        {'title': '战狼', 'year': '2019'},
+    ]
+
+    return render_template('index.html', name=name, movies=movies)
 
 
-# 动态路由
-@app.route('/index/<name>')
-def home(name):
-    return '<h1>hello %s</h1>'%name
+# # 动态路由
+# @app.route('/index/<name>')
+# def home(name):
+#     return '<h1>hello %s</h1>'%name
